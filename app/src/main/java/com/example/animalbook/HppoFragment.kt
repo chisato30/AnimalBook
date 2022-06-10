696e0a38afe03527ac2509f65bdf9effb420207e
chisato30 <moterunit@gmail.com>
2022-06-10 22:26:30 +0900

    ボタンタップでフラグメント表示処理

--- app/src/main/java/com/example/animalbook/HppoFragment.kt
+++ app/src/main/java/com/example/animalbook/HppoFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HippoFragment.newInstance] factory method to
+ * Use the [HppoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HippoFragment : Fragment() {
+class HppoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class HippoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hippo, container, false)
+        return inflater.inflate(R.layout.fragment_hppo, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class HippoFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HippoFragment.
+         * @return A new instance of fragment HppoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HippoFragment().apply {
+            HppoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/animalbook/SubActivity.kt
+++ app/src/main/java/com/example/animalbook/SubActivity.kt
@@ -2,10 +2,39 @@ package com.example.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.animalbook.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubBinding
+    private lateinit var title: TitleFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+        binding = ActivitySubBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+        binding.lionButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, LionFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        binding.hippoButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, HppoFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        binding.girraffeButon.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, GiraffeFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
     }
 }
